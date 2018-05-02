131647ceab708aa6407d61682f6a0054260981b0
G015C1140\G015C1140 <horizon12.kuro@gmail.com>
2018-05-02 18:47:23 +0900

    spajam道場で発表した時の最新版一式

--- F_roulette/app/src/main/java/com/example/g015c1140/f_roulette/MainActivity.java
+++ F_roulette/app/src/main/java/com/example/g015c1140/f_roulette/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.g015c1140.f_roulette;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.RotateAnimation;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -9,53 +16,74 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Iterator;
+import java.util.Random;
 
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity{
+    private ImageView imageView;
+    int rt, k = 0;
+    float toDegrees = 0;
+    Random rnd;
+    String result[] = new String[4];
+    Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-            // アクセスキー
-            String acckey = "a408fe819421bb02777124b05553557a";
-            // 緯度
-            String lat = "35.634165";
-            // 経度
-            String lon = "139.714698";
-            // 範囲
-            String range = "2";
-            // 返却形式
-            String format = "json";
-            //フリーワード
-            String freeWord = "そば";
-            // エンドポイント
-            String gnaviRestUri = "https://api.gnavi.co.jp/RestSearchAPI/20150630/";
-            String prmFormat = "?format=" + format;
-            String prmKeyid = "&keyid=" + acckey;
-            String prmLat = "&latitude=" + lat;
-            String prmLon = "&longitude=" + lon;
-            String prmRange = "&range=" + range;
-            String prmFreeWord = "&freeword=" + freeWord;
+        imageView = findViewById(R.id.imageView);
 
-            // URI組み立て
-            StringBuffer uri = new StringBuffer();
-            uri.append(gnaviRestUri);
-            uri.append(prmFormat);
-            uri.append(prmKeyid);
-            uri.append(prmLat);
-            uri.append(prmLon);
-            uri.append(prmRange);
-            uri.append(prmFreeWord);
+        Button buttonFadeOut = findViewById(R.id.start);
+        buttonFadeOut.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                rnd = new Random();
+                rt = rnd.nextInt(2);
 
-            // API実行、結果を取得し出力
-            getNodeList(uri.toString());
-        }
+                startRotationXml();
 
-   // }
 
-    private static void getNodeList(final String url) {
+
+                // アクセスキー
+                String acckey = "a408fe819421bb02777124b05553557a";
+                // 緯度
+                String lat = "35.634165";
+                // 経度
+                String lon = "139.714698";
+                // 範囲
+                String range = "3";
+                // 返却形式
+                String format = "json";
+                //フリーワード
+                String freeWord = result[rt];
+                // エンドポイント
+                String gnaviRestUri = "https://api.gnavi.co.jp/RestSearchAPI/20150630/";
+                String prmFormat = "?format=" + format;
+                String prmKeyid = "&keyid=" + acckey;
+                String prmLat = "&latitude=" + lat;
+                String prmLon = "&longitude=" + lon;
+                String prmRange = "&range=" + range;
+                String prmFreeWord = "&freeword=" + freeWord;
+
+                // URI組み立て
+                StringBuffer uri = new StringBuffer();
+                uri.append(gnaviRestUri);
+                uri.append(prmFormat);
+                uri.append(prmKeyid);
+                uri.append(prmLat);
+                uri.append(prmLon);
+                uri.append(prmRange);
+                uri.append(prmFreeWord);
+
+                // API実行、結果を取得し出力
+                getNodeList(uri.toString());
+            }
+        });
+    }
+
+
+
+    private void getNodeList(final String url) {
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -70,14 +98,16 @@ public class MainActivity extends AppCompatActivity{
 
                 } catch (Exception e){
                     System.out.println("例外が発生しました");
-                    //TODO: 例外を考慮していません
+                    Log.e("getNodeList", "error", e);
                 }
 
             }
         }).start();
     }
 
-    private static void viewJsonNode(JsonNode nodeList){
+
+
+    private void viewJsonNode(JsonNode nodeList){
         if(nodeList != null){
             //トータルヒット件数
             String hitcount   = "total:" + nodeList.path("total_hit_count").asText();
@@ -86,12 +116,68 @@ public class MainActivity extends AppCompatActivity{
             JsonNode restList = nodeList.path("rest");
             Iterator<JsonNode> rest = restList.iterator();
             //店舗名、店舗画像1のURL,PR文（短）を出力
+                //while(rest.hasNext()){
             JsonNode r = rest.next();
-            String name = r.path("name").asText();
-            String image1 = r.path("image_url").path("shop_image1").asText();
-            String pr = r.path("pr").path("pr_short").asText();
+            final String name = r.path("name").asText();
+            final String image1 = r.path("image_url").path("shop_image1").asText();
+            final String pr = r.path("pr").path("pr_short").asText();
             System.out.println("***************************************************");
             System.out.println("name: " + name + "\nimage1: " + image1 + "\npr: " + pr);
+              // }
+
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(MainActivity.this, com.example.g015c1140.f_roulette.OverviewActivity.class);
+                    intent.putExtra("name", name);
+                    intent.putExtra("image1", image1);
+                    intent.putExtra("pr", pr);
+                    startActivity(intent);
+                }
+            },2000);
+
         }
+    }
+
+
+    private void startRotationXml() {
+
+        //result[0] = "中華";
+        //result[1] = "焼き肉";
+        result[0] = "カレー";
+        result[1] = "すし";
+
+        rnd = new Random();
+        k = rnd.nextInt(8) + 8;
+        toDegrees = 360f * k;
+        switch (rt) {
+          /*  case 0:
+                toDegrees = toDegrees + 45;
+                break;
+            case 1:
+                toDegrees = toDegrees + 45 * 3;
+                break;
+                */
+            case 0:
+                toDegrees = toDegrees + 45 * 5;
+                break;
+            case 1:
+                toDegrees = toDegrees + 45 * 7;
+                break;
+        }
+        System.out.println(result[rt]);
+
+        RotateAnimation rotate = new RotateAnimation(0.0f, toDegrees, 30f, 290f);
+
+        // animation時間 msec
+        rotate.setDuration(1000);
+        // 繰り返し回数
+        rotate.setRepeatCount(0);
+        // animationが終わったそのまま表示にする
+        rotate.setFillAfter(true);
+
+        System.out.println(toDegrees);
+        imageView.startAnimation(rotate);
+
     }
 }
---
+++ F_roulette/app/src/main/java/com/example/g015c1140/f_roulette/MyView.java
@@ -0,0 +1,62 @@
+package com.example.g015c1140.f_roulette;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by bosanade on 2018/04/21.
+ */
+public class MyView extends View {
+
+    Paint paint1,paint2,paint3,paint4,paint5;
+    private Paint mPaint = new Paint();
+
+
+    public MyView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        paint1 = new Paint();
+        paint2 = new Paint();
+        paint3 = new Paint();
+        paint4 = new Paint();
+        paint5 = new Paint();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        RectF rectf1 = new RectF(0, 0, 720, 720);
+        RectF rectf2 = new RectF(0, 0, 720, 720);
+        RectF rectf3 = new RectF(0, 0, 720, 720);
+        RectF rectf4 = new RectF(0, 0, 720, 720);
+        RectF rectf5 = new RectF(0, 0, 180, 180);
+
+        paint1.setColor(Color.argb(255, 255, 236, 185));
+        paint2.setColor(Color.argb(255, 172, 226, 180));
+        paint3.setColor(Color.argb(255, 184, 178, 234));
+        paint4.setColor(Color.argb(255, 171, 231, 255));
+        paint5.setColor(Color.argb(255, 255, 255, 255));
+
+        rectf1.offset(0, 0);
+        rectf2.offset(0, 0);
+        rectf3.offset(0, 0);
+        rectf4.offset(0, 0);
+        rectf5.offset(270, 270);
+
+        canvas.drawArc(rectf1, 0, 90, true, paint1);
+        canvas.drawArc(rectf2, 90, 90, true, paint2);
+        canvas.drawArc(rectf3, 180, 90, true, paint3);
+        canvas.drawArc(rectf4, 270, 90, true, paint4);
+        canvas.drawArc(rectf5, 0, 360, true, paint5);
+
+        mPaint.setTextSize(50);
+
+        canvas.drawText("すし", 180, 180, mPaint);
+        canvas.drawText("カレー", 180, 540, mPaint);
+        canvas.drawText("中華", 450, 180, mPaint);
+        canvas.drawText("焼き肉", 450, 540, mPaint);
+    }
+}
